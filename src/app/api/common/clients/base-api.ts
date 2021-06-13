773143820e43bba6b04767fe0ee29978fa6e971c
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-06-13 22:42:02 +0100

    fix react api url

--- src/app/api/common/clients/base-api.ts
+++ src/app/api/common/clients/base-api.ts
@@ -4,7 +4,7 @@ import {setupAuthHandlers} from './common-api';
 const baseApiAxios = axios.create({});
 
 export const configureBaseApi = () => {
-    baseApiAxios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8070/api/';
+    baseApiAxios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://api.kalendario.ie/api/';
     setupAuthHandlers(baseApiAxios);
 }
 
